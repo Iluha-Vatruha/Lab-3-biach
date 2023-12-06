855382aee1867da038eb8c64a26f5ce99b6e64f5
Tony <toni.sas.228@mail.ru>
2023-12-06 19:28:31 +0800

    Also in Text\SolutionTypeHandler

--- src/solution/Text/SolutionTypeHandler.java
+++ src/solution/Text/SolutionTypeHandler.java
@@ -8,16 +8,19 @@ import java.util.Scanner;
 
 public class SolutionTypeHandler {
     public static Solution solutionType() {
+        Scanner s = new Scanner(System.in);
+        String option;
+        while (true){
             System.out.println("Введите задачу:");
             System.out.println("1. Заменить гласные на знак '-'.");
             System.out.println("2.Назад");
-            Scanner s = new Scanner(System.in);
-            String option = s.nextLine();
+
+            option = s.nextLine();
             switch (option){
                 case "1": return Solution.REPLACE_VOWELS;
                 case "2": return Solution.NONE;
                 default: System.out.println("Ты дебил? Введи нормально!"); break;
             }
-            return SolutionTypeHandler.solutionType();
+        }
     }
 }
